2330e5815c8ca68548db8a5e8d7c40bc4de5aea9
Vishvajeet-MT19AIE331 <141996908+Vishvajeet-MT19AIE331@users.noreply.github.com>
2023-11-03 13:09:39 +0530

    Update exp.py to select and save best models

--- exp.py
+++ exp.py
@@ -1,47 +1,12 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# pip install -r requirements.txt
-
-# Import datasets, classifiers and performance metrics
-from sklearn import metrics, svm
-
-from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
-from joblib import dump, load
-import pandas as pd
-
-num_runs  = 1
-# 1. Get the dataset
-X, y = read_digits()
-
-# 2. Hyperparameter combinations
-classifier_param_dict = {}
-# 2.1. SVM
-gamma_list = [0.0001, 0.0005, 0.001, 0.01, 0.1, 1]
-C_list = [0.1, 1, 10, 100, 1000]
-h_params={}
-h_params['gamma'] = gamma_list
-h_params['C'] = C_list
-h_params_combinations = get_hyperparameter_combinations(h_params)
-classifier_param_dict['svm'] = h_params_combinations
-
-# 2.2 Decision Tree
 max_depth_list = [5, 10, 15, 20, 50, 100]
 h_params_tree = {}
 h_params_tree['max_depth'] = max_depth_list
 h_params_trees_combinations = get_hyperparameter_combinations(h_params_tree)
 classifier_param_dict['tree'] = h_params_trees_combinations
-
+print()
+print('The combination of tested parameters for Decision Tree are:')
+print(h_params_trees_combinations)
+print('__________________________________________________________________________________________________________________')
 
 results = []
 test_sizes =  [0.2]
@@ -71,26 +36,28 @@ for cur_run_i in range(num_runs):
                 test_acc, test_f1, predicted_y = predict_and_eval(best_model, X_test, y_test)
                 train_acc, train_f1, _ = predict_and_eval(best_model, X_train, y_train)
                 dev_acc = best_accuracy
-
+                
+                print('BEST RESULTS:')
                 print("{}\ttest_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}, test_f1={:.2f}".format(model_type, test_size, dev_size, train_size, train_acc, dev_acc, test_acc, test_f1))
+                print('Best model:',best_model_path)
                 cur_run_results = {'model_type': model_type, 'run_index': cur_run_i, 'train_acc' : train_acc, 'dev_acc': dev_acc, 'test_acc': test_acc}
                 results.append(cur_run_results)
                 binary_preds[model_type] = y_test == predicted_y
                 model_preds[model_type] = predicted_y
                 
-                print("{}-GroundTruth Confusion metrics".format(model_type))
-                print(metrics.confusion_matrix(y_test, predicted_y))
+                #print("{}-GroundTruth Confusion metrics".format(model_type))
+                #print(metrics.confusion_matrix(y_test, predicted_y))
 
 
-print("svm-tree Confusion metrics".format())
-print(metrics.confusion_matrix(model_preds['svm'], model_preds['tree']))
+#print("svm-tree Confusion metrics".format())
+#print(metrics.confusion_matrix(model_preds['svm'], model_preds['tree']))
 
-print("binarized predictions")
-print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False]))
-print("binarized predictions -- normalized over true labels")
-print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='true'))
-print("binarized predictions -- normalized over pred  labels")
-print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='pred'))
+#print("binarized predictions")
+#print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False]))
+#print("binarized predictions -- normalized over true labels")
+#print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='true'))
+#print("binarized predictions -- normalized over pred  labels")
+#print(metrics.confusion_matrix(binary_preds['svm'], binary_preds['tree'], labels=[True, False] , normalize='pred'))
         
 # print(pd.DataFrame(results).groupby('model_type').describe().T)
-                
+print('__________________________________________________________________________________________________________________')
