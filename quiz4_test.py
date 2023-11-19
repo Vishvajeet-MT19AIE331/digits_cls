040ae3649176fb9e2d112e896bd222444b78c0a9
Vishvajeet-MT19AIE331 <141996908+Vishvajeet-MT19AIE331@users.noreply.github.com>
2023-11-19 15:29:13 +0530

    minor corrections

--- quiz4_test.py
+++ quiz4_test.py
@@ -22,14 +22,13 @@ def test_post_root():
     suffix = "post suffix"
     response = app.test_client().post("/predictor_post", json={"suffix":suffix})
     assert response.status_code == 200    
-    assert response.get_json()['op'] == "Hello, World POST "+suffix
-    assert (response.get_data() == b"<p>0</p>"+suffix or
-     response.get_data() == b"<p>1</p>"+suffix or
-     response.get_data() == b"<p>2</p>"+suffix or
-     response.get_data() == b"<p>3</p>"+suffix or
-     response.get_data() == b"<p>4</p>"+suffix or
-     response.get_data() == b"<p>5</p>"+suffix or
-     response.get_data() == b"<p>6</p>"+suffix or
-     response.get_data() == b"<p>7</p>"+suffix or
-     response.get_data() == b"<p>8</p>"+suffix or
-     response.get_data() == b"<p>9</p>" )
+    assert (response.get_json()['op'] == b"<p>0</p>"+suffix or
+     response.get_json()['op'] == b"<p>1</p>"+suffix or
+     response.get_json()['op'] == b"<p>2</p>"+suffix or
+     response.get_json()['op'] == b"<p>3</p>"+suffix or
+     response.get_json()['op'] == b"<p>4</p>"+suffix or
+     response.get_json()['op'] == b"<p>5</p>"+suffix or
+     response.get_json()['op'] == b"<p>6</p>"+suffix or
+     response.get_json()['op'] == b"<p>7</p>"+suffix or
+     response.get_json()['op'] == b"<p>8</p>"+suffix or
+     response.get_json()['op'] == b"<p>9</p>" )
