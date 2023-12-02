de4bdb5c0f3081844830f54f05f603b1b2cd4eb5
Vishvajeet-MT19AIE331 <141996908+Vishvajeet-MT19AIE331@users.noreply.github.com>
2023-12-02 11:18:11 +0530

    Correction to route in Flask appl

--- api/app.py
+++ api/app.py
@@ -1,24 +1,22 @@
 from flask import Flask, request
+import numpy as np
 from joblib import load
 
 app = Flask(__name__)
 # load the model for making predictions
+#model= load('C:/Users/vishv/OneDrive/Documents/GitHub/digits_cls/models/svm_gamma_0.001_C_1.joblib')
 model= load('/mnt/c/Users/vishv/OneDrive/Documents/GitHub/digits_cls/models/svm_gamma_0.001_C_1.joblib')
+#model= load('./models/svm_gamma_0.001_C_1.joblib')
 
-@app.route("/predictor/<image>")
-def predictor():
-    js=request.get_json()
-    image=js['image']
-    # convert image to array and make prediction using the model 
-    classification= model.predict(np.array(image).reshape(1, -1))
-       
-    return int(classification)    
-
-@app.route("/predict_post/<image>, methods=["POST"])
+@app.route("/", methods=["POST"])
 def predict_post():
     js=request.get_json()
     image=js['image']
     # convert image to array and make prediction using the model 
     classification= model.predict(np.array(image).reshape(1, -1))
     
-    return {"op" : int(classification) + request.json["suffix"]}
+    return {"The Model predicts that the image belongs to Class" : int(classification) }
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '0.0.0.0', port=5000)
